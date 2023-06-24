805224e7127cce13663664ca181b2ff46d2a46f4
Jimmy Huang <jimmyhuang454@gmail.com>
2023-06-24 09:44:44 +0800

    update

--- tools/install.sh
+++ tools/install.sh
@@ -1,19 +1,22 @@
+quit(){
+  echo $1
+  exit 1
+}
+
 download_lastest_RRS_to() {
-  DOWNLOAD_LINK="https://github.com/JimmyHuang454/RRS/releases/download/latest/RRS_Linux.zip"
+  DOWNLOAD_LINK="https://github.com/JimmyHuang454/RRS/releases/latest/download/RRS_Linux.zip"
   echo "Downloading from: $DOWNLOAD_LINK"
   if ! curl -R -H 'Cache-Control: no-cache' -o "$1"  "$DOWNLOAD_LINK"; then
-    echo 'error: Download failed! Please check your network or try again.'
-    exit 1
+    quit 'error: Download failed! Please check your network or try again.'
   fi
 }
 
 decompression() {
-  SAVED_PATH="./step2/"
-  if ! unzip -q "$1" -d $SAVED_PATH ; then
-    echo 'error: decompression failed.'
-    exit 1
+  if ! unzip -q "$1" ; then
+    quit 'error: decompression failed.'
   fi
 }
 
 download_lastest_RRS_to "./step1.zip"
 decompression "./step1.zip"
+ls disk
