a498bcaeaba806fda588a3a8ee7b581fe547e90b
Jimmy Huang <jimmyhuang454@gmail.com>
2023-06-24 09:02:38 +0800

    test

--- tools/install.sh
+++ tools/install.sh
@@ -1,7 +1,6 @@
 download_lastest_RRS_to() {
-  # DOWNLOAD_LINK="https://github.com/JimmyHuang454/RRS/releases/download/latest/RRS_Linux.exe"
-  DOWNLOAD_LINK="https://github.com/2dust/v2rayN/releases/download/6.27/v2rayN-32.zip"
-  echo "Downloading archive: $DOWNLOAD_LINK"
+  DOWNLOAD_LINK="https://github.com/JimmyHuang454/RRS/releases/download/latest/RRS_Linux.zip"
+  echo "Downloading from: $DOWNLOAD_LINK"
   if ! curl -R -H 'Cache-Control: no-cache' -o "$1"  "$DOWNLOAD_LINK"; then
     echo 'error: Download failed! Please check your network or try again.'
     exit 1
@@ -12,12 +11,9 @@ decompression() {
   SAVED_PATH="./step2/"
   if ! unzip -q "$1" -d $SAVED_PATH ; then
     echo 'error: decompression failed.'
-    "rm" -r "$SAVED_PATH"
-    echo "removed: $SAVED_PATH"
     exit 1
   fi
-  echo "info: Extract the package to $SAVED_PATH and prepare it for installation."
 }
 
-# download_lastest_RRS_to "./files1.zip"
+download_lastest_RRS_to "./step1.zip"
 decompression "./step1.zip"
